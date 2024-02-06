31cac94673fa79f46033c23189ddda507558f031
Pp <Pimolnat_ka@cmu.ac.th>
2024-02-06 16:23:50 +0700

    lab17_2.cpp completed

--- lab17_2.cpp
+++ lab17_2.cpp
@@ -1,19 +1,24 @@
 #include<iostream>
+#include "lab17_2.h"
 using namespace std;
 
-________ myString(_____________________){
-	_______ = new ____________;    
-	for(int i = 0; i < N;i++) ____________ = 'A'+i;
-	__________ = 0;
+void myString(char *&x,int N){
+	x = new char[N];    
+	for(int i = 0; i < N;i++) x[i] = 'A'+i;
+	x[N] = 0;
 }
 
 int main(){
 	char *p;
 	int N;	
 	cout << "N = ";
-	cin >> N;		
-	myString(p,N);
-	cout << p;	
+	cin >> N;
+    NewFunction(p, N);
+    cout << p;	
 	delete [] p;
 	return 0;
+}
+void NewFunction(char *&p, int N)
+{
+    myString(p, N);
 }
